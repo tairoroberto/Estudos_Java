c6f573dc3e3a1ea04435f3e764ccc72a0d57b33f
tairoroberto <tairoroberto@hotmail.com>
2016-01-02 20:48:59 -0200

    Modifier final

---
+++ src/Modificadores.java
@@ -0,0 +1,16 @@
+/**
+ * Created by tairo on 02/01/16.
+ */
+public class Modificadores {
+
+    public static void main(String[] args) {
+
+        int populacaoBrasileira = 203429773;
+
+        //name ever in Upper case an value never change
+        final double PI = 3.1415926535897932846;
+
+        System.out.println("PI: " + PI );
+
+    }
+}
