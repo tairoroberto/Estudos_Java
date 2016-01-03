4771f9cef16f377e302d7eb4a52f1ef1df930737
tairoroberto <tairoroberto@hotmail.com>
2016-01-03 12:30:35 -0200

    Operators in Java

---
+++ src/br/com/trmasolucoes/logica/Operators.java
@@ -0,0 +1,16 @@
+package br.com.trmasolucoes.logica;
+
+/**
+ * Created by tairo on 03/01/16.
+ */
+public class Operators {
+    public static void main(String[] args) {
+
+        // 2 + 3 // binary operator
+        // -2 // unary operator
+        // true ? "yes" : "no" // ternary operator
+
+        int x = 9 + 4;
+        System.out.println("Result: " + x);
+    }
+}
