952c199037ec96d1fdd00755a4ce2d955a4d557a
tairoroberto <tairoroberto@hotmail.com>
2016-01-03 10:40:29 -0200

    Data entry with Scanner in Java

--- src/br/com/trmasolucoes/logica/DataEntry.java
+++ src/br/com/trmasolucoes/logica/DataEntry.java
@@ -1,5 +1,6 @@
 package br.com.trmasolucoes.logica;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 /**
@@ -11,6 +12,13 @@ public class DataEntry {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
-        System.out.println("Entrada: " + s.nextLine());
+        System.out.println("Type your name:\n");
+        String nome = s.nextLine();
+        System.out.println("Hello " + nome);
+
+        // Data entry with graphical window
+        nome = JOptionPane.showInputDialog("What is your name?");
+        System.out.println("Hello more one time: " + nome);
+        JOptionPane.showMessageDialog(null,"Hello more one time " + nome);
     }
 }
