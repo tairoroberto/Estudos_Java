e097a7e07c0d614c9876ee7ad2dde61491b1ce08
tairoroberto <tairoroberto@hotmail.com>
2016-01-03 18:05:21 -0200

    Mathematics operators

--- src/br/com/trmasolucoes/logica/Operators.java
+++ src/br/com/trmasolucoes/logica/Operators.java
@@ -12,5 +12,23 @@ public class Operators {
 
         int x = 9 + 4;
         System.out.println("Result: " + x);
+
+        /** Mathematic Operators*/
+        x = 2 + 3; // Sun
+        x = 2 - 3; // Subtract
+        x = 2 * 3; // Multiply
+        x = 2 / 3; // Divide
+        x = 4 % 2; // Module
+        x = +4; // unary operator
+        x = -4; // unary operator
+        int y = 4;
+        x = ++y; //pre-increment operator
+        x = --y; //pre-decrement operator
+        x = y++; //post-increment operator
+        x = y--; //post-decrement operator
+
+
+
+        System.out.println(x);
     }
 }
